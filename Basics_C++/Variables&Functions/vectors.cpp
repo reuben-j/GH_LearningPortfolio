4c84ee1ef0cad58d914d27f5236666e02f250709
reuben-j <reubenjarvela@gmail.com>
2024-07-02 19:48:20 +0100

    MoreVectors

--- Basics_C++/Variables&Functions/vectors.cpp
+++ Basics_C++/Variables&Functions/vectors.cpp
@@ -11,7 +11,7 @@ int main()
     
 
 
-    vector<int> newVect;
+    vector<int> newVect; // see below for more .push_back() info 
     newVect.push_back(10);
     newVect.push_back(20);
     newVect.push_back(30);
@@ -59,26 +59,58 @@ int main()
 
 
     //TODO vect.empty()
-        // cout << vect.empty();
+        //! cout << vect.empty();
     //* returns a true or false to whether or not the vector is empty
 
 
 
     //TODO vect.size()
-        // cout << vect.size();
+        //! cout << vect.size();
     //* Returns the numbers of elements in the vector
 
 
 
     //TODO (  =  ) equals operator
-    vector<int> myVect1 = {1, 2, 3};
+    vector<int> myVect1 = {1, 2 };
     vector<int> myVect2 = {4, 5, 6, 7};
-    myVect1 = myVect2;
+    //! cout << myVect1.size() << endl; // cout size of original vector with size of 2
+    myVect1 = myVect2; // assign new contents and allocate necessary storage
     for(vector<int>::iterator it = myVect1.begin(); it != myVect1.end(); ++it) {
-        cout << " " << *it;
+        //! cout  << *it << " "; // output = 4 5 6 7
         }
-    // output = 4 5 6 7
+    //! cout << endl << myVect1.size(); // cout new size of 4
+    //* Allows for the reassignment of vectors using the = operator. This automatically readjusts size of container and maintains order. 
 
+
+
+    //TODO (  []  ) square brackets operator
+    //! cout << myVect2[0]; //output 4
+    //* Cout the exact element with the given reference
+
+
+
+    //TODO vector.front()
+    //! cout << myVect2.front(); //output 4
+    //* Allow you to specifically get reference to first element of the vector
+
+
+
+    //TODO vector.back()
+    //! cout << myVect2.back(); //output 7
+    //* Allow you to specifically get reference to the last element of the vector
+
+
+
+    //TODO vector.push_back()
+        vector<int> myVect3 = {1, 2, 3, 4};
+        for(vector<int>::iterator x = myVect3.begin(); x != myVect3.end(); ++x) {
+            //! cout << *x << " ";
+        } // output 1 2 3 4 
+        myVect3.push_back(5);
+        for(vector<int>::iterator x = myVect3.begin(); x != myVect3.end(); ++x) {
+            //! cout << *x << " ";
+        } // output 1 2 3 4 5
+    //* Allow you to directly place a new element at the back of the vector and automatically increase container size
 }
 
 // to update code runner to allow c++20 features, update json file using this link for info: 
