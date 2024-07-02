fa378bf68932d17669b5f5fd898fd89bb7639d14
reuben-j <reubenjarvela@gmail.com>
2024-07-02 19:03:21 +0100

    VectorUpdates

--- Basics/vectors.cpp
+++ Basics/vectors.cpp
@@ -1,17 +1,40 @@
-// C++ program to initialize 
-// a vector like an array.
 #include <iostream>
 #include <vector>
 using namespace std;
 
 int main()
 {
-    vector<int> vect { 10, 20, 30 };
+    //! declaring a vector 
 
-    for (int x : vect)
-        cout << x << " ";
+    vector<int> vect = {1, 2, 3, 4};
+    //* basic declaration with elements displayed from start
+    
 
-    return 0;
+    vector<int> newVect;
+    newVect.push_back(10);
+    newVect.push_back(20);
+    newVect.push_back(30);
+    //* push_back puts the newest element at the back and moves everything else up +1
+    
+
+    vector<int> newerVect(3, 10);
+    //* declare a vector of size 3 with each element having the value of 10
+    
+    
+    int arr[] = {10, 20, 30};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    vector<int> newererVect(arr, arr + n);
+    //* initialise a vector via the use of a string
+
+
+    vector<int>vect2(vect.begin(), vect.end());
+    //* using the top vector, initialise a vector from another whole vector   
+
+
+    vector<int> vectNewest(10);
+    int value = 4;
+    fill(vectNewest.begin(), vectNewest.end(), value);
+    //* using the fill function, turns all values to the specified value (4 in this case)
 }
 
 // to update code runner to allow c++20 features, update json file using this link for info: 
