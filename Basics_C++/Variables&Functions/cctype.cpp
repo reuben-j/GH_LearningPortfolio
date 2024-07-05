9ea757dd4c5a768075606b3f33a325fc8d6ede8d
reuben-j <reubenjarvela@gmail.com>
2024-07-05 09:30:16 +0100

    isdigit()

--- Basics_C++/Variables&Functions/cctype.cpp
+++ Basics_C++/Variables&Functions/cctype.cpp
@@ -16,11 +16,23 @@ if(isalnum(cha) == true) {
 
 
 
-//TODO 
+//TODO isalpha()
 char alpha = '1';
 if(isalpha(alpha) == true) {
     cout << alpha << " " << "This is a letter" << endl;
 } else {
     cout << alpha << " " << "This is not a letter" << endl;
 }
+//* Return bool whether or not variable is alphabetic
+
+
+
+//TODO isdigit()
+char num = '9';
+if(isdigit(num)) {
+    cout << num << " " << "This is a number";
+} else {
+    cout << num << " " << "This is not a number";
+}
+//* Returns bool whether or not variable is a decimal digit or not
 }
