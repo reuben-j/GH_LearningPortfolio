b7f27d7364649e610e741e35711e1e24794bf9dd
reuben-j <reubenjarvela@gmail.com>
2024-07-05 11:49:05 +0100

    regex

--- Basics_C++/Variables&Functions/regex.cpp
+++ Basics_C++/Variables&Functions/regex.cpp
@@ -4,5 +4,11 @@
 using namespace std;
 
 int main() {
-    cout << "hey";
+    string str1 = "Hello there my friend";
+    regex a("(Hello)(.*)");
+    regex b("(my)(.*)");
+
+    if((regex_match(str1, a) && (regex_match(str1, b)))) {
+        cout << "Match for Hello";
+    } 
 }
