bdc35da1ca3f2645bcff48cbfe18c18320c20ad1
reuben-j <reubenjarvela@gmail.com>
2024-07-06 13:02:16 +0100

    regex_search

--- Basics_C++/Variables&Functions/regex.cpp
+++ Basics_C++/Variables&Functions/regex.cpp
@@ -16,11 +16,22 @@ int main() {
     //'.' can not be on its own, if its alone it will interpreted as part of the searched for string
     
     if (regex_match(text, pattern)) {
-        cout << "The text matches the pattern!" << std::endl;
+        cout << "The text matches the pattern!" << endl;
     } else {
-        cout << "The text does not match the pattern." << std::endl;
+        cout << "The text does not match the pattern." << endl;
     } // output - The text matches the pattern
     
+    
    
-    //
+    //TODO regex_search
+    string text2[] = {"are", "bare", "ware", "aadarekaadw", "aasdasdasd"};
+
+    regex pattern2("a.*e");
+    int found = 0;
+    for(auto text : text2) {
+    if(regex_search(text, pattern2)) {
+        found++;
+    } 
+    }
+    cout << found << " / " << sizeof(text2) / sizeof(text2[0]) << " are matches!";
 }
