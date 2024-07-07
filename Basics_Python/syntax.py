da2255a91d5c1e066bf97029f433340fb8e75561
reuben-j <reubenjarvela@gmail.com>
2024-07-07 15:41:46 +0100

    VariableDeclation

--- Basics_Python/syntax.py
+++ Basics_Python/syntax.py
@@ -23,3 +23,21 @@ str1 = "Hello mate"
 str2 = 'Hiya buddy'
 print(str1, str2) #output: Hello mate Hiya buddy
 
+#variable names are case sensitive, the following are two different variables
+a = 23
+A = 24
+print(a, A) #output: 23 24
+
+#delcare multiple variables at once
+str3, str4, str5 = "hiya", "dude", "what up"
+print(str3, str4, str5) #ouput: hiya dude what up
+
+#assign one value to multiple varaibles
+num1 = num2 = num3 = 3
+print(num1, num2, num3) #outuput 3 3 3
+
+#you can 'unpack' a collection of variable and assign each to a var. shown below from in the tuple
+
+fruits = ["apple", "banana", "orange"]
+fruit1, fruit2, fruit3 = fruits
+print(fruit1, fruit2, fruit3) #output: apple banana orange
