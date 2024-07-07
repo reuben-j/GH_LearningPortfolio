ba897bc20b9ebc13fa07acb881588b283c1f696a
reuben-j <reubenjarvela@gmail.com>
2024-07-07 15:33:59 +0100

    variables Python

---
+++ Basics_Python/syntax.py
@@ -0,0 +1,25 @@
+#This is how to comment
+
+#Declaring variables - variables do not need to be declared with a type and can even be changed following initial declaration
+name = "Reuben"
+print (name) #output: Reuben
+name = "Abi"
+print  (name) # ouput: Abi
+
+age = 24 # int var 
+height = 183.2 # float or double var
+cha = 'A' #character variable
+
+# you can type cast a certain variable type when setting if need be
+x = int(33)
+y = str("Hiya")
+
+#printing type of variable
+print(type(x)) # output: <class 'int'>
+print (type(height))
+
+#strings can be declared with single or double quotes
+str1 = "Hello mate"
+str2 = 'Hiya buddy'
+print(str1, str2) #output: Hello mate Hiya buddy
+
