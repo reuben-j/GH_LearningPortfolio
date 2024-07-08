929dc5e0fb1103735544693129286ffe46887ce0
reuben-j <reubenjarvela@gmail.com>
2024-07-08 13:07:50 +0100

    Comments

--- Basics_C++/Files/filesTest.cpp
+++ Basics_C++/Files/filesTest.cpp
@@ -7,7 +7,9 @@ int main() {
     // create and open a text file
     ofstream MyFile("filename.txt");
 
+    // write to file
     MyFile << "I am testing, this should write to the file I created above!";
 
+    // close file
     MyFile.close();
 }
