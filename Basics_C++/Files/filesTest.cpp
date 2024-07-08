0eb358ec350cc0564b3275bce0fe3d00c3598c83
reuben-j <reubenjarvela@gmail.com>
2024-07-08 13:18:02 +0100

    Files

--- Basics_C++/Files/filesTest.cpp
+++ Basics_C++/Files/filesTest.cpp
@@ -8,8 +8,22 @@ int main() {
     ofstream MyFile("filename.txt");
 
     // write to file
-    MyFile << "I am testing, this should write to the file I created above!";
+    MyFile << "I am testing, this should write to the file I created above! \n";
+    MyFile << "This should be on the next line!";
 
-    // close file
+    // close file - good practice
     MyFile.close();
+
+    // create string to use when opening file
+    string readFromFile;
+
+    // read from the text file we made earlier
+    ifstream MyReadFile("filename.txt");
+
+    // use while loop and and getline function to ready file line by line
+    while(getline (MyReadFile, readFromFile)) {
+        cout << readFromFile << "\n"; //outputs text from file
+    }
+
+    MyReadFile.close();
 }
