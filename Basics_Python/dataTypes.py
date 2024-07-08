bc957b36f28ee46ce2b59dd99d744b0917556f2d
reuben-j <reubenjarvela@gmail.com>
2024-07-08 15:17:17 +0100

    DataTypes

---
+++ Basics_Python/dataTypes.py
@@ -0,0 +1,31 @@
+# Data types
+
+# text type
+str1 = str("Reuben")
+
+# Numerics types
+num = int(2)
+num1 = float(3.31)
+num2 = complex(102838231.12312312)
+
+#Sequence types
+
+#lists use square brackets
+myList = [12, 23, 34]
+
+#tuples are written with round brackets
+#tuples is a collection which is ordered and unchangeable
+myTuple = ("apple", "date", "banana")
+
+#sets are written with curly braces
+#set items are unchangeable. but can remove and add items 
+mySet = {123, 321, 231}
+
+#dictionaries are used to store data in key:value pairs
+thisDict = {
+    "brand": "Ford",
+    "model": "GT",
+    "year": 1978
+}
+print(thisDict) #to print the whole dictionary
+print(thisDict["brand"]) #to print a particular item
