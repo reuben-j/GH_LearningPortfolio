54b17ae8f72a3133642f4d5a9951b873fed2068f
reuben-j <reubenjarvela@gmail.com>
2024-07-08 16:33:51 +0100

    SliceString

--- Basics_Python/strings.py
+++ Basics_Python/strings.py
@@ -11,3 +11,17 @@ print(str1)
 str2 = '''Woah look there's only singles now!
 how good!'''
 print(str2)
+
+#slice a string with the following function
+str3 = "Hello world!"
+print(str3[2:5]) #prints the chars starting at index 2 to 5
+#output is 'llo'
+
+#you can slice from the start also without a beginnign number
+print(str3[:5]) #output 'Hello'
+
+#slice to the end from a certain index
+print(str3[2:]) #output 'llo world!'
+
+#slice from the end of the string by using '-'
+print(str3[-6:-1])
