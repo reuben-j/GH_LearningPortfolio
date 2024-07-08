c371ec09079ccc7f713fbcc50c3807deee041e6a
reuben-j <reubenjarvela@gmail.com>
2024-07-08 16:29:02 +0100

    UpdateCommit

---
+++ Basics_Python/strings.py
@@ -0,0 +1,13 @@
+import random
+
+#using random library, print random letter from 1 to 10
+print(random.randrange(1, 10))
+
+#you can create multiline string variables using three double or single quote marks """
+str1 = """Hi there mate
+how ya going? """
+print(str1)
+
+str2 = '''Woah look there's only singles now!
+how good!'''
+print(str2)
