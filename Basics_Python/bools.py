acecadd6761d95f6bea0522e18dd90f24f87d58f
reuben-j <reubenjarvela@gmail.com>
2024-07-09 12:06:11 +0100

    Bools

--- Basics_Python/bools.py
+++ Basics_Python/bools.py
@@ -17,4 +17,25 @@ else:
 #evaluate values and variables
 print(bool(15 > 103)) # false
 
-# almost all values are true
+# almost all values are true. Empty strings are false. Int with value 0 is false.
+# any list, tuple, set and dictionary are true unless empty
+
+print(bool(None)) # false
+print(bool(0)) # false
+print(bool("")) # false
+print(bool({})) # false
+
+# a function can return a boolean value
+def myBoolFunc():
+    return True
+
+print(myBoolFunc()) # True
+
+if(myBoolFunc()):
+    print("Func is true") # <- this one is outputted
+else: 
+    print("My func false")
+    
+# isinstance(var, type) returns true if the target variable is the same type as the target type
+num3 = 100
+print(isinstance(num3, str)) #false because num3 is not a string
