cffb20abf0aa6ee1dd85c4fd2b930bd725bec680
reuben-j <reubenjarvela@gmail.com>
2024-07-09 11:59:53 +0100

    FirstCommitForBools

---
+++ Basics_Python/bools.py
@@ -0,0 +1,20 @@
+# operators
+print(8 > 10) # false
+print(8 >= 10) # false
+print(8 == 10) # false
+print(8 <= 10) # true
+print(8 < 10) # true
+
+#if statement
+num1 = 10
+num2 = 5
+if num1 > num2:
+    print(num1, "is greater than ", num2)
+else:
+    print(num2, "is greater than ", num1)
+# output 10 is greater than 5
+
+#evaluate values and variables
+print(bool(15 > 103)) # false
+
+# almost all values are true
