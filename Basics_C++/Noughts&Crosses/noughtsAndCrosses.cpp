fbf3171559cdd1ea704ca66248878cb8e4511547
reuben-j <reubenjarvela@gmail.com>
2024-07-09 16:37:08 +0100

    WorkingWithoutWinCondition

--- Basics_C++/Noughts&Crosses/noughtsAndCrosses.cpp
+++ Basics_C++/Noughts&Crosses/noughtsAndCrosses.cpp
@@ -8,24 +8,28 @@ using namespace std;
 
 int PlayerTurnHeight (char player) {
     int height;
-    cout << "Your turn " << player << endl << "From top, would you like row 0, 1 or 2?" << endl;
+    cout << "Your turn " << player << ":" << endl << "From top, would you like row 1, 2 or 3?" << endl;
     cin >> height;
-    if(height == 1) {
-        height += 1;
+    if(height <= 1) {
+        height = 0;
     } else if(height == 2) {
-        height *= 2;
+        height = 2;
+    } else if(height >=3) {
+        height = 4;
     }
     return height;
     }
 
 int PlayerTurnWidth (char player) {
     int width;
-    cout << "From left to right, would you like column 0, 1 or 2?" << endl;
+    cout << "From left to right, would you like column 1, 2 or 3?" << endl;
     cin >> width;
-    if(width == 1) {
-        width += 1;
+    if(width <= 1) {
+        width = 0;
     } else if(width == 2) {
-        width *= 2;
+        width = 2;
+    } else if(width >= 3) {
+        width = 4;
     }
     return width;
     }
@@ -43,10 +47,12 @@ vector<vector<char>> gameBoardPrint(vector<vector<char>> gameBoard) {
 }
 
 int main() {
-    // available spaces for chars
-    // top line -    [0][0] - [0][2] - [0][4]
-    // middle line - [2][0] - [2][2] - [2][4]
-    // bottom line - [4][0] - [4][2] - [4][4]
+    char player1, player2;
+    cout << "First player, what character would you like to use? ";
+    cin >> player1;
+    cout << "Second player, what character would you like to use? ";
+    cin >> player2;
+    
 
 vector<vector<char>> board = {
      {' ', '|', ' ', '|', ' '},
@@ -57,24 +63,30 @@ vector<vector<char>> board = {
     };
 
 gameBoardPrint(board);
-
+// loop through 9 turns to fill each spot 
 for(int i = 0; i < 9; i++) {
     char player;
-    if(i % 2 == 0) {
-        player = 'X';
+    if(i % 2 == 0) { //even
+        player = player1;
     } else {
-        player = 'O';
+        player = player2;
     }
     int turn1Height = PlayerTurnHeight(player);
     int turn1Width = PlayerTurnWidth(player);
 
     if(board[turn1Height][turn1Width] != ' ') {
-        cout << endl << "That spot is taken!" << endl;
+        cout << endl << "**************************" << endl << endl <<
+        "That spot is taken!" << endl;
+
     int turn1Height = PlayerTurnHeight(player);
     int turn1Width = PlayerTurnWidth(player);
-    } else {
+    } 
+
     board[turn1Height][turn1Width] = player;
     gameBoardPrint(board);
+    if(i == 8) {
+        cout << "Its a draw!";
+        break;
     }
 }   
 }
