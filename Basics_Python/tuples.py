0442c77bf7399904d00ccf9d5e0bc485ed59bfb1
reuben-j <reubenjarvela@gmail.com>
2024-07-10 11:01:35 +0100

    tuplesCreation

---
+++ Basics_Python/tuples.py
@@ -0,0 +1,7 @@
+# a tuple is a collection which is ordered and unchangeable
+# tuples are written with round brackest
+# tuple items are ordered, unchangeable, and allow duplicate values.
+
+
+myTuple = (12, 23, 34)
+print(myTuple)
