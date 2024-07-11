c3a985543983c17712a1e364ec41549a4aaac911
reuben-j <reubenjarvela@gmail.com>
2024-07-11 04:59:44 +0100

    Tuples

--- Basics_Python/tuples.py
+++ Basics_Python/tuples.py
@@ -27,3 +27,25 @@ print(type(newTuple), newTuple) # <class 'tuple'> (12, 23, 34, 45)
 #access tuple specifically with range index
 tuple1 = (123, 234, 345, 456, 456, 567, 567, 678, 678)
 print(tuple1[3:]) #(456, 456, 567, 567, 678, 678)
+
+#tuples are unchangeable, if you want to change the value of a tuple,
+# change it to a list, update value, change back to a tuple
+tuple2 = {"Reuben", "Abigail", "Marcus"}
+list1 = list(tuple2)
+list1[0] = "Jordan"
+tuple2 = tuple(list1)
+print(tuple2) #('Jordan', 'Reuben', 'Marcus')
+
+#tuples are immutable, they can not be added to. You can convert to a list
+#add values, then convert back to a tuple
+tuple3 = {1, 2, 3, 4, 5}
+list2 = list(tuple3)
+list2.append(True)
+tuple3 = tuple(list2)
+print(tuple3) #(1, 2, 3, 4, 5, True)
+
+# you can add tuples together
+tuple4 = (21, 32, 43, 54)
+tuple5 = ("Me", "You", "them")
+tuple4 += tuple5
+print(tuple4) #(21, 32, 43, 54, 'Me', 'You', 'them')
