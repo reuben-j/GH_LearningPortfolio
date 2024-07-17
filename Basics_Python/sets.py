4894427f75a473f7a271f651c09922228cb5219a
reuben-j <reubenjarvela@gmail.com>
2024-07-17 13:04:11 +0100

    unordered, no duplicates

--- Basics_Python/sets.py
+++ Basics_Python/sets.py
@@ -1,4 +1,12 @@
 # sets are a collection of values that are unordered, unchangeable and unindexed
+#set items are unchangeable, but you can add and remove items
 # sets are declared with curly braces
 mySet = {12, 23, 34}
 print(mySet) #{34, 12, 23}
+
+# sets do not allow for duplicates
+mySet01 = {123, 123, 123, 12}
+print(mySet01) #{123, 12}
+
+#! print(mySet01[0]) #this throws an error because sets are unordered, you can not reference by index
+
