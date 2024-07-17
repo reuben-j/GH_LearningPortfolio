f28c8e8765b0fb99059b56251c9eba2585b5f954
reuben-j <reubenjarvela@gmail.com>
2024-07-17 13:08:48 +0100

    SetStuff

--- Basics_Python/sets.py
+++ Basics_Python/sets.py
@@ -10,3 +10,16 @@ print(mySet01) #{123, 12}
 
 #! print(mySet01[0]) #this throws an error because sets are unordered, you can not reference by index
 
+# values 1 and True are considered duplicates, same for 0 and False
+mySet02 = {1, True, 0, False}
+print(mySet02) #{0, 1}
+
+#get length of set
+print(len(mySet02)) #2
+
+#sets can contain different data types
+mySet03 = {1, "2", False}
+print(mySet03) #{False, 1, '2'}
+
+#python recognises sets as:
+print(type(mySet03)) #<class 'set'>
