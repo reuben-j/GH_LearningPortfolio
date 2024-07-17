a13da90e59b890b271b5fa0dc571e20244664cc7
reuben-j <reubenjarvela@gmail.com>
2024-07-17 13:18:45 +0100

    LoopThrough&AddToSet

--- Basics_Python/sets.py
+++ Basics_Python/sets.py
@@ -23,3 +23,22 @@ print(mySet03) #{False, 1, '2'}
 
 #python recognises sets as:
 print(type(mySet03)) #<class 'set'>
+
+#use the constructor set(("item1", "item2")) with double parenthesis
+mySet04 = set((12, 23, 34, 45))
+for x in mySet04:
+    print(x) #34 12 45 23
+    
+# cannot access items directly in set, instead can check whether a value is present or not
+if 12 in mySet04:
+    print("True") #True
+
+#check if item is not in set
+if 122 not in mySet04:
+    print("122 not present") #122 not present
+
+#you cannot change existing values of set, but can add items
+mySet05 = {"Reuben", "Abi", "Marcus"}
+mySet05.add("Iris")
+print(mySet05) #{'Abi', 'Iris', 'Marcus', 'Reuben'}
+
