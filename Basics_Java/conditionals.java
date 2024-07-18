48da46e326edef6214f7e96692c353c8285b2cc2
reuben-j <reubenjarvela@gmail.com>
2024-07-18 22:33:04 +0100

    Conditionals

---
+++ Basics_Java/conditionals.java
@@ -0,0 +1,13 @@
+public class conditionals {
+    public static void main(String[] args) {
+        int num = 50;
+        int num2 = 25;
+        if (num < num2) {
+            System.out.println("num less than num2");
+        } else if (num == num2) {
+            System.out.println("There are equal");
+        } else {
+            System.out.println("Num2 is smaller than num");
+        }
+    }
+}
