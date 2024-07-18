a404d762a46e40f845ab2ac52a7d801e65c649ed
reuben-j <reubenjarvela@gmail.com>
2024-07-18 17:59:58 +0100

    printAndVariables

--- Basics_Java/test.java
+++ Basics_Java/test.java
@@ -1,6 +1,20 @@
 public class test{ // class name must always match filename
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) { // any code inside main will be executed. Main must be included in every program
+        System.out.print("Hello world!\n"); // print to console
+        System.out.println("Test"); // using print over println will mean a new line is not added after line is printed
+        System.out.println(123 + 123); // print a number with double quotes. Math can be done here also
+
+        int num = 15;
+        System.out.println(num); //15
+
+        String name = "Reuben";
+        System.out.println(name); // Reuben
+
+        char letter; // declare variable without allocating value
+        letter = 'A';
+        System.out.println(letter); // A
+
+
     }
 }
 
@@ -9,7 +23,7 @@ public class test{ // class name must always match filename
 //* e.g javac test.java
 
 // to run java code
-// java 'filename'\
+// java 'filename'
 //* e.g java test
 
 // to run a java file from terminal, first open a textedit doc, convert to basic text (not rich one) and save with file type of java
