2a1d2ca3bc847301d38ed74cc2d37f35f7fe027d
reuben-j <reubenjarvela@gmail.com>
2024-07-18 22:58:29 +0100

    methods()

---
+++ Basics_Java/methods.java
@@ -0,0 +1,9 @@
+public class methods {
+
+    static void myMethod() {
+        System.out.println("Working function!");
+    }
+    public static void main(String[] args) {
+        myMethod();
+    }
+}
