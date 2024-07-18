a84de8e1d9e34f4b10032056a306d45c0bee4928
reuben-j <reubenjarvela@gmail.com>
2024-07-18 22:47:11 +0100

    Loops

--- Basics_Java/conditionals.java
+++ Basics_Java/conditionals.java
@@ -27,8 +27,41 @@ public class conditionals {
                 System.out.println("He is another age!");
         }
 
+        // while loop
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i); // 0 1 2 3 4
+            i++;
+        }
 
+        // do while loop
+        int x = 0;
+        do {
+            System.out.println("Still going!");
+            x++;
+        } while (x < 3); //x3 Still going!
 
+        // for loop
+        for(int y = 0; y < 4; y++) {
+            System.out.println(y); // 0 1 2 3
+        }
+        int q = 0, w = 0;
+        // nested loop
+        for(int a = 0; a < 5; a++) {
+            System.out.print("a:");
+            System.out.println(q);
+            q++;
+            for(int b = 0; b < 2; b++) {
+                System.out.print("b:");
+                System.out.println(w);
+                w++;
+            }
+        }
 
+        // for each loop
+        int array[] = {12, 23, 34, 45};
+        for (int I : array) {
+            System.out.println(I); // 12 23 34 45
+        }
     }
 }
