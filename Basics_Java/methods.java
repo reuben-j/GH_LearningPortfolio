cfccc993769b94fc544dc3c82379c7c3a258c33e
reuben-j <reubenjarvela@gmail.com>
2024-07-21 16:21:18 +0100

    MoreMethods

--- Basics_Java/methods.java
+++ Basics_Java/methods.java
@@ -14,10 +14,18 @@ public class methods {
             System.out.println("I can afford it!");
         }
     }
+
+    static int returnSum(int a, int b) {
+        return a + b;
+    }
     public static void main(String[] args) {
         myMethod();
         myMethod();
         newMethod(13, 45); // call them here
         checkPrice(34);
+        System.out.println(returnSum(123, 234)); // return value inside print line
+        int sum = returnSum(10, 90); // allocate variable value with function
+        System.out.println(returnSum(13, 23));
+        System.out.println(sum);
     }
 }
