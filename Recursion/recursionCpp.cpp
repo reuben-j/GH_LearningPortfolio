efefa5d54b82a1eb7bdcab5ce2a4bc95af59983d
reuben-j <reubenjarvela@gmail.com>
2024-07-21 23:11:37 +0100

    Recursion

---
+++ Recursion/recursionCpp.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int factorial(int num) {
+    if (num <= 1){
+        return 1;
+    }
+    else {
+        return num * factorial(num - 1);
+    }
+}
+int main() {
+    int num = 5;
+    cout << factorial(num);
+}
