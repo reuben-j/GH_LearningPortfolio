149a1a0d7ae986f3a944b24eb4d40bd7ec37a840
reuben-j <reubenjarvela@gmail.com>
2024-07-21 17:31:08 +0100

    recursion

---
+++ Recursion/recursionJava.java
@@ -0,0 +1,13 @@
+public class recursionJava {
+
+    static int factorial(int num) {
+        if(num <= 1) {
+            return num;
+        } else {
+            return num * factorial(num - 1);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(factorial(8));
+    }
+}
