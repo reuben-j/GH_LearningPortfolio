8e39d591187ebd31150bd0f48bd4e26e52f95c97
reuben-j <reubenjarvela@gmail.com>
2024-07-21 16:15:41 +0100

    moreMethods

--- Basics_Java/methods.java
+++ Basics_Java/methods.java
@@ -3,12 +3,21 @@ public class methods {
     static void myMethod() {
         System.out.println("Working function!");
     }
-    static void newMethod(int i, int j) {
+    static void newMethod(int i, int j) { //pass parameters to a function here
         System.out.println(i + j);
+    }
+
+    static void checkPrice(int price) { // use an if statement inside the 
+        if(price >= 50) {
+            System.out.println("It costs too much!");
+        } else {
+            System.out.println("I can afford it!");
+        }
     }
     public static void main(String[] args) {
         myMethod();
         myMethod();
-        newMethod(13, 45);
+        newMethod(13, 45); // call them here
+        checkPrice(34);
     }
 }
