09f866480f5685581e7b02ce76f652e7699e88c7
reuben-j <reubenjarvela@gmail.com>
2024-07-21 09:34:42 +0100

    methods

--- Basics_Java/methods.java
+++ Basics_Java/methods.java
@@ -3,8 +3,12 @@ public class methods {
     static void myMethod() {
         System.out.println("Working function!");
     }
+    static void newMethod(int i, int j) {
+        System.out.println(i + j);
+    }
     public static void main(String[] args) {
         myMethod();
         myMethod();
+        newMethod(13, 45);
     }
 }
