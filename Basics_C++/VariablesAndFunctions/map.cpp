26e21d1ddf911de889b9e42c14adab26d59c7737
reuben-j <reubenjarvela@gmail.com>
2024-07-22 14:04:06 +0100

    moreMaps

--- Basics_C++/VariablesAndFunctions/map.cpp
+++ Basics_C++/VariablesAndFunctions/map.cpp
@@ -17,12 +17,31 @@ int main() {
     // cout << myMap["one"];
 
     // get an iterator pointing to first element of map
-    map<string, string>::iterator it = myMap.begin();
+    // map<string, string>::iterator it = myMap.begin();
 
-    // iterate through map
-    while (it != myMap.end()) { // while the iterator does not equal last member of map
-        cout << "Key: " << it->first // print key then first part of iterator member
-            << " - Value: " << it->second << endl; // print second part of first iterator
-        ++it; // increase iteratory count to look at next key/value pair
+    // // iterate through map
+    // while (it != myMap.end()) { // while the iterator does not equal last member of map
+    //     cout << "Key: " << it->first // print key then first part of iterator member
+    //         << " - Value: " << it->second << endl; // print second part of first iterator
+    //     ++it; // increase iteratory count to look at next key/value pair
+    // }
+
+
+
+    map<string, string>passwordMap;
+    string passwordId, passwordItself;
+    cout << "What is the name of the password you would like to store? " << endl;
+    cin >> passwordId;
+    cout << "What is your password? " << endl;
+    cin >> passwordItself;
+
+    passwordMap[passwordId] = passwordItself;
+
+    map<string, string>::iterator Iteratory = passwordMap.begin();
+
+    while(Iteratory != passwordMap.end()) {
+         cout << "Name: " << Iteratory->first 
+            << " - Password: " << Iteratory->second << endl; 
+        ++Iteratory;
     }
 }
---
+++ Basics_C++/VariablesAndFunctions/tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+map<string, string>::iterator it = myMap.begin();
+
+    // // iterate through map
+    // while (it != myMap.end()) { // while the iterator does not equal last member of map
+    //     cout << "Key: " << it->first // print key then first part of iterator member
+    //         << " - Value: " << it->second << endl; // print second part of first iterator
+    //     ++it; // increase iteratory count to look at next key/value pair
+    // }
