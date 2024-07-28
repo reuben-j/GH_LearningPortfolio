6b8721d9f9d2dbb402ba3328b205483fbd39ba52
reuben-j <reubenjarvela@gmail.com>
2024-07-28 18:33:03 +0100

    Commit

---
+++ Basics_C++/OOP/oopBasics.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+class MyClass { // class declaration and naming
+    public: //access specifier
+        int age;        //attribute
+        string name;    //attribute
+};
+
+int main() {
+
+}
