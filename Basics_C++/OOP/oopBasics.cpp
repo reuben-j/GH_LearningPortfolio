d398c64d42f9e37c2a839ca11a39f339972ced75
reuben-j <reubenjarvela@gmail.com>
2024-07-29 06:01:20 +0100

    OOP

--- Basics_C++/OOP/oopBasics.cpp
+++ Basics_C++/OOP/oopBasics.cpp
@@ -6,8 +6,17 @@ class MyClass { // class declaration and naming
     public: //access specifier
         int age;        //attribute
         string name;    //attribute
+        void printMethod() {
+            cout << "I am from a class method!";
+        }
 };
 
 int main() {
+
+    MyClass newObjReuben; // object creation from class
+
+    newObjReuben.age = 24; // attribute value declaration
+    newObjReuben.name = "Reuben";// attribute value declaration
+    newObjReuben.printMethod(); // call class method
 
 }
