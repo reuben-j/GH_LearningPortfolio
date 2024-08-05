d8e67aaa29afe7d4290683b297c720e2af3924fe
reuben-j <reubenjarvela@gmail.com>
2024-08-05 06:25:28 +0100

    StringFormatting

--- Basics_C++/Libraries/format.cpp
+++ Basics_C++/Libraries/format.cpp
@@ -5,7 +5,9 @@ using namespace std;
 
 int main() {
 
-
-
+    int age = 24;
+    string name = "Reuben";
+    string formatted_Str = format("My name is {} and I am {} years old", name, age);
+    cout << formatted_Str;
     return 0;
 }
