4b067332ac1fc241d4686becdc269be1ff2ea732
reuben-j <reubenjarvela@gmail.com>
2024-08-06 04:57:27 +0100

    MoreFormats

--- Basics_C++/Libraries/format.cpp
+++ Basics_C++/Libraries/format.cpp
@@ -8,6 +8,10 @@ int main() {
     int age = 24;
     string name = "Reuben";
     string formatted_Str = format("My name is {} and I am {} years old", name, age);
-    cout << formatted_Str;
+    cout << formatted_Str << endl;
     return 0;
+
+    int num1 = 654;
+    string myStr = format("My least favourite number is {}", num1);
+    cout << myStr << endl;
 }
