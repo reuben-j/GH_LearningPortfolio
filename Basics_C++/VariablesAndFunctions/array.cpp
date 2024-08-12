d0940ecdc8a193abd1dbb59fa10da4d67202b513
reuben-j <reubenjarvela@gmail.com>
2024-08-12 19:24:21 +0100

    ArrayStuff

--- Basics_C++/VariablesAndFunctions/array.cpp
+++ Basics_C++/VariablesAndFunctions/array.cpp
@@ -4,11 +4,20 @@ using namespace std;
 
 int main() {
     int numArray[] = {132, 41, 9, 1242, 764, 55};
+
+    
     int size = sizeof(numArray) / sizeof(numArray[0]); 
-    sort(numArray, numArray+size);
 
     for(int i = 0; i < size; i++) {
         cout << numArray[i] << " ";
     }
+    cout << endl;
+    sort(numArray, numArray+size, greater<int>());
+
+    for(int i = 0; i < size; i++) {
+        cout << numArray[i] << " ";
+    }
+
+    
     
 }
