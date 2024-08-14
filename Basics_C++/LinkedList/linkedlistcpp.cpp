cc2960e610317983fee804ae0faabb12b9f9ee7e
reuben-j <reubenjarvela@gmail.com>
2024-08-14 13:15:15 +0100

    Comments

--- Basics_C++/LinkedList/linkedlistcpp.cpp
+++ Basics_C++/LinkedList/linkedlistcpp.cpp
@@ -2,24 +2,26 @@
 
 using namespace std;
 
-struct Node {
-        int dataNum;
-        Node* next;
+struct Node { // define a structure of node type
+        int dataNum; // data stored
+        Node* next; // node pointer that points to next member in list
 };
 
 class linkedlist {
     private:
-        Node* head;
+        Node* head; // Pointer to the first node of the list. If list empty, head will be nullptr
     public:
-        linkedlist() : head(nullptr) {}
-
-        ~linkedlist() {
-            Node* current = head;
-            Node* next;
-            while (current != nullptr) {
-                next = current->next;
-                delete current;
-                current = next;
+        linkedlist() :// constructor of the linkedlist class, called when linkedlist object created
+        head(nullptr) {} //allocated head of list to nullptr on construction
+        
+        //destructor can be identified by the ~ symbol
+        ~linkedlist() { // this is called when a list member is deleted or goes out of scope
+            Node* current = head; // starts by finding top of list at head
+            Node* next; // declares value for what will be used for next in list
+            while (current != nullptr) { // checks if the current has a value or if is nullptr
+                next = current->next; // allocates next pointer
+                delete current; // deletes data from current
+                current = next; // moves to next member in list
             }
         }
 
