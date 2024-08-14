de51dbd6045a225c2f535aa7337faf3d3a55c70c
reuben-j <reubenjarvela@gmail.com>
2024-08-14 15:46:36 +0100

    Commit

--- Basics_C++/VariablesAndFunctions/mod.cpp
+++ Basics_C++/VariablesAndFunctions/mod.cpp
@@ -3,6 +3,12 @@
 using namespace std;
 
 int main() {
-    cout << 3 % 6;
-    
+    cout << 51 / 10;
+
+    int num1 = 3;
+    int num2 = 5;
+
+    string str = to_string(num1) + to_string(num2);
+    cout << str;
+
 }
