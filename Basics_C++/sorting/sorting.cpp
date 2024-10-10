3ba1054aea731363288b1417cdb3fc44fa74963d
reuben-j <reubenjarvela@gmail.com>
2024-10-10 14:37:07 +0100

    yep

--- Basics_C++/sorting/sorting.cpp
+++ Basics_C++/sorting/sorting.cpp
@@ -7,28 +7,29 @@ int main()
 {
 
     vector<int> myVec = {112, 23, 313, 12, 842, 13, 10, 4};
-    
+    vector<int> newVec;
+    newVec.reserve(myVec.size());
 
+    cout << "Unsorted: ";
     for (auto i : myVec)
     {
         cout << i << " ";
     }
 
     cout << '\n';
-    int temp = myVec[0];
-    cout << "Index 0: " << myVec[0] << "\n";
-    int min = 0, max = 0;
+    int min = myVec[0];
+    int index;
 
     for (int i = 1; i < myVec.size(); i++)
     {
-        
-        cout << myVec[i] << " Vs " << temp << "\n";
-        
-        if (myVec[i] < temp)
-        {
-            temp = myVec[i];
-        } 
-        cout << "Min: " << temp << "\n";
 
+        cout << myVec[i] << " Vs " << min << "\n";
+
+        if (myVec[i] < min)
+        {
+            min = myVec[i];
+            index = i;
+        }
     }
+    cout << "Min: " << min << "\n";
 }
---
+++ Basics_C++/sorting/tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+newVec.insert(newVec[i], min);
