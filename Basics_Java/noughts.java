af792702c813d64c4499f4d5996989a07e2e1836
reuben-j <reubenjarvela@gmail.com>
2024-10-10 11:45:25 +0100

    Donezo

--- Basics_Java/noughts.java
+++ Basics_Java/noughts.java
@@ -26,15 +26,14 @@ public class noughts {
                     int vertTemp = playerPrompt(playerOne);
 
                     while (numArr[horiTemp - 1][vertTemp - 1] == 1) {
-                        System.out.println("*** Spot taken, try again. ***");
-                        System.out.print("------------------------------");
+                        tryAgain();
                         printArr(arr);
                         horiTemp = playerPrompt(playerTwo);
                         vertTemp = playerPrompt(playerTwo);
                     }
                     playerTurn(arr, numArr, horiTemp, vertTemp, playerOne);
                     printArr(arr);
-                    gameWon = checkWin(arr, playerOne, playerTwo);
+                    gameWon = checkWin(arr, playerOne);
                     if (gameWon == true)
                         gameWinner = playerOne;
                 } else {
@@ -43,75 +42,65 @@ public class noughts {
 
                     // System.out.println(numArr[horiTemp][vertTemp]);
                     while (numArr[horiTemp - 1][vertTemp - 1] == 1) {
-                        System.out.println("*** Spot taken, try again. ***");
-                        System.out.print("------------------------------");
+                        tryAgain();
                         printArr(arr);
                         horiTemp = playerPrompt(playerTwo);
                         vertTemp = playerPrompt(playerTwo);
-
                     }
                     playerTurn(arr, numArr, horiTemp, vertTemp, playerTwo);
-
                     printArr(arr);
-                    gameWon = checkWin(arr, playerOne, playerTwo);
+                    gameWon = checkWin(arr, playerTwo);
                     if (gameWon == true)
                         gameWinner = playerTwo;
                 }
             }
-
         }
 
         printArr(arr);
         if (gameWon == true) {
-            System.out.println("Well done player: " + gameWinner + ", you won the game!");
+            System.out.println("Well done player: " + gameWinner + "! You won the game!");
         } else {
             System.out.println("Draw!");
         }
     }
 
     static void printArr(char arr[][]) {
-
         System.out.println("------\n");
+        System.out.println("*********");
         for (int i = 0; i < 5; i++) {
+            System.out.print("* ");
             for (int y = 0; y < 5; y++) {
                 System.out.print(arr[i][y]);
             }
+            System.out.print(" *");
             System.out.println();
         }
+        System.out.println("*********");
         System.out.println("\n------\n");
     }
 
     static int[][] playerTurn(char arr[][], int numArr[][], int hori, int vert, char player) {
 
-        int horiNumArr = 0, vertNumArr = 0;
+        numArr[hori - 1][vert - 1] = 1;
         if (hori == 1) {
             hori = 0;
-            horiNumArr = 0;
         }
         if (hori == 2) {
             hori = 2;
-            horiNumArr = 1;
         }
         if (hori == 3) {
             hori = 4;
-            horiNumArr = 2;
         }
         if (vert == 1) {
             vert = 0;
-            vertNumArr = 0;
         }
         if (vert == 2) {
             vert = 2;
-            vertNumArr = 1;
         }
-
         if (vert == 3) {
             vert = 4;
-            vertNumArr = 2;
         }
-
         arr[hori][vert] = player;
-        numArr[horiNumArr][vertNumArr] = 1;
         System.out.println('\n');
         return numArr;
     }
@@ -132,49 +121,39 @@ public class noughts {
         return horiChoice;
     }
 
-    static boolean checkWin(char gameBoard[][], char player1, char player2) {
+    static boolean checkWin(char gameBoard[][], char player) {
 
-        if (gameBoard[0][0] == player1 && gameBoard[0][2] == player1 && gameBoard[0][4] == player1) {
+        if (gameBoard[0][0] == player && gameBoard[0][2] == player && gameBoard[0][4] == player) {
             return true;
-        } else if (gameBoard[0][0] == player2 && gameBoard[0][2] == player2 && gameBoard[0][4] == player2) {
+        } else if (gameBoard[2][0] == player && gameBoard[2][2] == player && gameBoard[2][4] == player) {
             return true;
-        } else if (gameBoard[2][0] == player1 && gameBoard[2][2] == player1 && gameBoard[2][4] == player1) {
-            return true;
-        } else if (gameBoard[2][0] == player2 && gameBoard[2][2] == player2 && gameBoard[2][4] == player2) {
-            return true;
-        } else if (gameBoard[4][0] == player1 && gameBoard[4][2] == player1 && gameBoard[4][4] == player1) {
-            return true;
-        } else if (gameBoard[4][0] == player2 && gameBoard[4][2] == player2 && gameBoard[4][4] == player2) {
+        } else if (gameBoard[4][0] == player && gameBoard[4][2] == player && gameBoard[4][4] == player) {
             return true;
         }
         // left to right win conditions for both ^
-        else if (gameBoard[0][0] == player1 && gameBoard[2][0] == player1 && gameBoard[4][0] == player1) {
+        else if (gameBoard[0][0] == player && gameBoard[2][0] == player && gameBoard[4][0] == player) {
             return true;
-        } else if (gameBoard[0][0] == player2 && gameBoard[2][0] == player2 && gameBoard[4][0] == player2) {
+        } else if (gameBoard[0][2] == player && gameBoard[2][2] == player && gameBoard[4][2] == player) {
             return true;
-        } else if (gameBoard[0][2] == player1 && gameBoard[2][2] == player1 && gameBoard[4][2] == player1) {
-            return true;
-        } else if (gameBoard[0][2] == player2 && gameBoard[2][2] == player2 && gameBoard[4][2] == player2) {
-            return true;
-        } else if (gameBoard[0][4] == player1 && gameBoard[2][4] == player1 && gameBoard[4][4] == player1) {
-            return true;
-        } else if (gameBoard[0][4] == player2 && gameBoard[2][4] == player2 && gameBoard[4][4] == player2) {
+
+        } else if (gameBoard[0][4] == player && gameBoard[2][4] == player && gameBoard[4][4] == player) {
             return true;
         }
         // top to bottom win conditions for both ^
-        else if (gameBoard[0][0] == player1 && gameBoard[2][2] == player1 && gameBoard[4][4] == player1) {
+        else if (gameBoard[0][0] == player && gameBoard[2][2] == player && gameBoard[4][4] == player) {
             return true;
-        } else if (gameBoard[0][0] == player2 && gameBoard[2][2] == player2 && gameBoard[4][4] == player2) {
-            return true;
-        } else if (gameBoard[0][4] == player1 && gameBoard[2][2] == player1 && gameBoard[4][0] == player1) {
-            return true;
-        } else if (gameBoard[0][4] == player2 && gameBoard[2][2] == player2 && gameBoard[4][0] == player2) {
+        } else if (gameBoard[0][4] == player && gameBoard[2][2] == player && gameBoard[4][0] == player) {
             return true;
         } else
             // diagonal win conditions for both ^
-
             return false;
+    }
 
+    static void tryAgain() {
+        System.out.println("******************************");
+        System.out.println("*** Spot taken, try again. ***");
+        System.out.println("******************************");
+        System.out.print("------------------------------");
     }
 
 }
