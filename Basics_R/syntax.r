995998f73fd58f6fc4ae9e37e15a924534f566c4
reuben-j <reubenjarvela@gmail.com>
2024-10-31 18:26:25 +0000

    okyes

--- Basics_R/syntax.r
+++ Basics_R/syntax.r
@@ -11,3 +11,31 @@ plot(1:10)
 
 # basic calculations
 1 + 2
+
+# variable assignment
+name <- "Reuben"
+age <- 24
+
+# auto print variables
+name
+age
+
+#paste() function
+phrase <- "Sick!"
+paste("R is", phrase)
+
+#paste() more
+str1 <- "Hello"
+str2 <- "mate!"
+paste(str1, str2)
+
+num1 <- 123
+num2 <- 321
+num1 + num2
+
+#assign multiple variable in one line
+var1 <- var2 <- var3 <- "Chilling"
+paste(var1, "yeah", var2, "mega", var3)
+
+#class()
+class(var1) #character
