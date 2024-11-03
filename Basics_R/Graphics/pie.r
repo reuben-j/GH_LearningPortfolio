fa4ccbb8f361089b4c6a995a33ba1c3a02d6c8f6
reuben-j <reubenjarvela@gmail.com>
2024-11-03 06:23:15 +0000

    yerp

---
+++ Basics_R/Graphics/pie.r
@@ -0,0 +1,4 @@
+# create pie charts with pie()
+
+x <- c(10, 20, 30, 40)
+pie(x)
--- Basics_R/Graphics/plot.r
+++ Basics_R/Graphics/plot.r
@@ -59,5 +59,5 @@ x2 <- c(2, 2, 8, 1, 15, 8, 12, 9, 7, 3, 11, 4, 7, 14, 12)
 y2 <- c(100, 105, 84, 105, 90, 99, 90, 95, 94, 100, 79, 112, 91, 80, 85)
 
 plot(x1, y1, main = "Observation of Cars", xlab = "Car age",
-     ylab = "Car speed", col = "red", cex = 2)
+     ylab = "Car speed", col = "#0a6500", cex = 2)
 points(x2, y2, col = "blue", cex = 2)
